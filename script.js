a744ba7e8eecbb141d7476ef016d06d54ef3925e
daq131 <daq131@gmail.com>
2022-02-08 12:31:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.guess').value = '';
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  //
+  if (!guess) {
+    displayMessage('No Number');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number');
+    //document.querySelector('.highscore').textContent = score;
+
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+      //
+    } else {
+      displayMessage('You losT the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
